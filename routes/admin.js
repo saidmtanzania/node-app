9c6133a98e9416bc140ced363f59efd89f04f2d5
saidmtanzania <saidmtanzania@gmail.com>
2022-06-19 17:13:48 +0300

    dynamic files changes

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,8 @@ const express = require("express");
 const rootDir = require("../util/path");
 const router = express.Router();
 
+const products = [];
+
 router.get("/add-product", (req, res, next) => {
   res.sendFile( path.join(rootDir, 'views', 'add-product.html'));
 });
@@ -13,4 +15,5 @@ router.post("/add-product", (req, res) => {
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
