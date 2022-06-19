4f50ac931fa4e767abd3340ccb5248999ed5e11c
saidmtanzania <saidmtanzania@gmail.com>
2022-06-19 17:42:59 +0300

    initial changes

--- routes/admin.js
+++ routes/admin.js
@@ -11,7 +11,7 @@ router.get("/add-product", (req, res, next) => {
 });
 
 router.post("/add-product", (req, res) => {
-  console.log(req.body);
+  products.push({title: req.body.title});
   res.redirect("/");
 });
 
