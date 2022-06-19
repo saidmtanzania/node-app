3dea1ce2f5a1e085653b128e9e02f66a8a2d4a82
saidmtanzania <saidmtanzania@gmail.com>
2022-06-19 20:13:19 +0300

    finalize pug

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404',{ docTitle: '404'});
 });
 app.listen(3000);
 // app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
