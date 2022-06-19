fdb2e2ffac53d36ba42fe2db3193b69ffcde11af
saidmtanzania <saidmtanzania@gmail.com>
2022-06-19 20:49:56 +0300

    initializing main-layout.pug to every page

--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,7 @@ const products = [];
 
 router.get("/add-product", (req, res, next) => {
   // res.sendFile( path.join(rootDir, 'views', 'add-product.html'));
-  res.render('add-product', { docTitle: 'Add Product'});
+  res.render('add-product', { docTitle: 'Add Product', path: 'admin'});
 });
 
 router.post("/add-product", (req, res) => {
