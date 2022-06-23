e5d107fde5bbab7177d209f0fabefc1e7899ef52
saidmtanzania <saidmtanzania@gmail.com>
2022-06-23 22:15:17 +0300

    finalizing Controller parts

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //jshint esversion:6
-const path = require('path');
 const express = require('express'); 
 const bodyParser = require('body-parser');
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -17,9 +17,9 @@ app.use(express.static('public'));
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404',{ docTitle: '404'});
-});
+app.use(errorController.get404);
+
+
+
 app.listen(3000);
 // app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
