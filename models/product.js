a228516e2bf3ac108f1297e3a54c471f38ddb63b
saidmtanzania <saidmtanzania@gmail.com>
2022-06-24 20:32:18 +0300

    initials changes

--- models/product.js
+++ models/product.js
@@ -1,13 +1,15 @@
 // jshint esversion:6
-
-const products = [];
+// const products = [];
+const fs = require('path');
+const path = require('path');
 
 module.exports = class Product {
     constructor(t) {
         this.title = t;
     }
     save(){
-        products.push(this);
+        // products.push(this);
+        const p = path.join(path.dirname(process.mainModule.filename));
     }
 
     static fetchAll(){
