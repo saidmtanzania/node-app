ef8459b89d18387d655234827261fba2f6590f67
saidmtanzania <saidmtanzania@gmail.com>
2022-06-29 13:21:23 +0300

    huh muh

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,15 +1,46 @@
 // jshint esversion:6
 const Product = require('../models/product');
 
+
+//Shop Home Page
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      prods: products,
+      docTitle: "My Shop",
+      path: "/"
+    });
+  });
+};
+
+
+//Shop List of Product Page
 exports.getProducts = (req, res, next) => {
   Product.fetchAll( products => {
     res.render("shop/product-list", {
       prods: products,
-      docTitle: "My Shop",
-      path: "/",
-      hasProduct: products.length > 0,
-      activeShop: true,
-      productCSS: true,
+      docTitle: "All Products",
+      path: "/products"
     });
+  });
+};
+
+
+// Shop Cart Page
+exports.getCart = (req, res, next) => {
+    res.render("shop/cart", {
+      prods: products,
+      docTitle: "Shop Cart",
+      path: "/cart"
+    });
+};
+
+
+//Shop Checkout Page
+exports.getCheckout = (req, res, next) => {
+  res.render("shop/cart", {
+    prods: products,
+    docTitle: "Shop Checkout",
+    path: "/checkout"
   });
 };
--- routes/shop.js
+++ routes/shop.js
@@ -3,9 +3,9 @@ const express = require("express");
 const shopController = require('../controllers/shop');
 const router = express.Router();
 
-router.get("/", shopController.getProducts);
+router.get("/", shopController.getIndex);
 
-router.get('/products');
+router.get('/products', shopController.getProducts);
 
 router.get('/cart');
 
