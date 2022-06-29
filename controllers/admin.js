3632e1c972c1096e97d10d254aa8addc28697e74
saidmtanzania <saidmtanzania@gmail.com>
2022-06-29 15:09:54 +0300

    initial changes

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,12 @@ exports.getAddProducts = (req, res, next) => {
 };
 
 exports.postAddProducts = (req, res, next) => {
-  const product = new Product(req.body.title);
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+
+  const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect("/");
 };
