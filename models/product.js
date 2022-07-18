27135f2b0305641e4ae12f52188a0e49e19e1ee4
saidmtanzania <saidmtanzania@gmail.com>
2022-07-18 12:06:03 +0300

    initial changes

--- models/product.js
+++ models/product.js
@@ -42,4 +42,11 @@ module.exports = class Product {
     static fetchAll( cb ){
       getProductsFromFile(cb);
     }
+
+    static findById(id, cb){
+      getProductsFromFile(products =>{
+        const product = products.find(p => p.id === id);
+        cb(product);
+      });
+    }
 };
