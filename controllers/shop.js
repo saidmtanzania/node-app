6858905ccc5c5e6c67d209f485ba271c4a725b12
saidmtanzania <saidmtanzania@gmail.com>
2022-07-18 12:12:40 +0300

    initial changes

--- controllers/shop.js
+++ controllers/shop.js
@@ -28,9 +28,10 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
+    res.render('shop/product-detail',{prod:product});
     console.log(product);
   });
-  res.redirect('/');
+  
 };
 
 // Shopping Cart Page
