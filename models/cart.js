937b0ef9603ba4b6b1b1f9f45c832057c342cf3b
saidmtanzania <saidmtanzania@gmail.com>
2022-07-19 12:39:23 +0300

    init

--- models/cart.js
+++ models/cart.js
@@ -1,5 +1,6 @@
 // jshint esversion:11
 
+const { createSecretKey } = require('crypto');
 const fs = require('fs');
 const path = require('path');
 
@@ -24,18 +25,26 @@ module.exports = class Cart {
           }
 
           //Analyze the cart => Find existing products
-          const existingProduct = cart.products.find((prod) => prod.id === id);
+          const existingProductIndex = cart.products.findIndex((prod) => prod.id === id);
+          const existingProduct = cart.products[existingProductIndex];
           let updatedProduct;
-          
+
           // Add new Product/increase quantity
           if (existingProduct) {
             updatedProduct = { ...existingProduct };
             updatedProduct.qty = updatedProduct.qty + 1;
+            cart.products = [...cart.products];
+            cart.products[existingProductIndex] = updatedProduct;
           }else{
             updatedProduct = { id: id, qty: 1};
+            cart.products = [...cart.products, updatedProduct];
           }
-          cart.totalPrice = cart.totalPrice + productPrice;
 
+          cart.totalPrice = cart.totalPrice + productPrice;
+          fs.writeFile(p, JSON.stringify(cart), err => {
+            console.log(err);
+          });
+          
         });
     }
 };
