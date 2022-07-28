f6dfeadf9bb46e78ec75a201f54ce74c85779674
saidmtanzania <saidmtanzania@gmail.com>
2022-07-28 21:18:15 +0300

    sequelize configuration

--- util/database.js
+++ util/database.js
@@ -1,11 +1,8 @@
 //jshint esversion:6
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'nodeDB',
-    password: ''
+const sequelize = new Sequelize("nodeDB", "root", "", {
+  dialect: "mysql",
+  host: "localhost",
 });
-
-module.exports = pool.promise();
+module.exports = sequelize;
