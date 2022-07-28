7f2c0811997e65454102a398857185f5aea4f927
saidmtanzania <saidmtanzania@gmail.com>
2022-07-28 21:48:28 +0300

    initial changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express'); 
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
+const sequelize = require("./util/database");
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
@@ -19,7 +21,14 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch((errr) => {
+    console.log(err);
+  });
 
-
-app.listen(3000);
 // app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
--- models/product.js
+++ models/product.js
@@ -1,6 +1,31 @@
 // jshint esversion:6
 const Sequelize = require('sequelize');
 
-const sequelize = require('../util/database');
+const Product = sequelize.define("product", {
+  id: { 
+    type: Sequelize.INTEGER, 
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
 
-const Product = sequelize.
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = Product;
